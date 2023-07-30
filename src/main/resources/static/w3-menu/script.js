ab8be4ff8f6c29ed9ad09bd5da1d4d6d1a745a2b
Lucian Nicoara <mail.lucian@gmail.com>
2023-07-30 12:36:58 +0300

    sc

--- src/main/resources/static/w3-menu/script.js
+++ src/main/resources/static/w3-menu/script.js
@@ -4,7 +4,12 @@ $(window).on('resize', function () {
 	if($(window).width() > 700){
 		$("#mySidenav").width("250px");
 		$("#main").css("margin-left", "250px");
+		$("#btnMenu").fadeOut(500);
+		$("#btnCloseSidenav").fadeOut(500);
+		
 	}else{
+		$("#btnMenu").fadeIn(500);
+		$("#btnCloseSidenav").fadeIn(500);
 		if(!sideNavOpened){
 			$("#mySidenav").width("0px");
 			$("#main").css("margin-left", "0px");
