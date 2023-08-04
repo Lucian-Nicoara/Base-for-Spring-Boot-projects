8211b7f1e55f81172e3835372d29a285a95faf1f
Lucian Nicoara <mail.lucian@gmail.com>
2023-08-04 20:18:39 +0300

    templating with fragments

---
+++ src/main/java/com/lucian/base/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.lucian.base.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
--- src/main/java/com/lucian/base/controller/IndexController.java
+++ src/main/java/com/lucian/base/controller/IndexController.java
@@ -25,7 +25,26 @@ public class IndexController {
 		List<UserData> users = userDataProcess.getUserData();
 		model.addAttribute("name", users.get(0).getName());
 		
-		return "index";
+		return "index.html";
 	}
 
+	@GetMapping("/template.html")
+	public String getTemplate(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+		System.out.println("debug: into getTemplate");
+
+		List<UserData> users = userDataProcess.getUserData();
+		model.addAttribute("name", users.get(0).getName());
+
+		return "template.html";
+	}
+	
+	@GetMapping("/content1.html")
+	public String getContent1(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+		System.out.println("debug: into getContent1");
+
+		List<UserData> users = userDataProcess.getUserData();
+		model.addAttribute("name", users.get(0).getName());
+
+		return "content1.html";
+	}
 }
