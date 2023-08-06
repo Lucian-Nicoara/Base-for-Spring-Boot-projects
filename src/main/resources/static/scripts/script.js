6684a2bccd578dd4846672ab1736d8c9c5c9ad88
Lucian Nicoara <mail.lucian@gmail.com>
2023-08-06 23:33:27 +0300

    fixed user menu

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -39,12 +39,3 @@ function closeNavOver() {
 	$("#btnMenu").show();
 	$("#btnCloseSidenav").hide();
 }
-
-/* global bootstrap: false */
-(() => {
-  'use strict'
-  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  tooltipTriggerList.forEach(tooltipTriggerEl => {
-    new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-})()
