5c62cfe141cba825fdfa1655e91ffe8ababc88eb
Lucian Nicoara <mail.lucian@gmail.com>
2023-08-20 10:22:25 +0300

    wip

--- src/main/java/com/lucian/base/controller/IndexController.java
+++ src/main/java/com/lucian/base/controller/IndexController.java
@@ -20,31 +20,20 @@ public class IndexController {
 	
 	@GetMapping("/")
 	public String getIndex(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		System.out.println("debug: into getIndex");
-		
 		List<UserData> users = userDataProcess.getUserData();
 		model.addAttribute("name", users.get(0).getName());
-		
 		return "pages/homepage.html";
 	}
 	
 	@GetMapping("/signup.html")
 	public String signup(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-		System.out.println("debug: into signup");
-
-		List<UserData> users = userDataProcess.getUserData();
-		model.addAttribute("name", users.get(0).getName());
-
+		model.addAttribute("name", "Lucian");
 		return "pages/signup.html";
 	}
 	
 	@GetMapping("/signin.html")
 	public String signin(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-		System.out.println("debug: into signin");
-
-		List<UserData> users = userDataProcess.getUserData();
-		model.addAttribute("name", users.get(0).getName());
-
+		model.addAttribute("name", "Lucian");
 		return "pages/signin.html";
 	}
 }
