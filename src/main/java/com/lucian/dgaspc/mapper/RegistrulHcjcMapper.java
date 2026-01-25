ea6f371cfcb2a0916938f2c6391dfbee321b27f1
Lucian <Lucian@Lucian-PC>
2026-01-25 22:56:09 +0200

    wip

--- src/main/java/com/lucian/dgaspc/mapper/RegistrulHcjcMapper.java
+++ src/main/java/com/lucian/dgaspc/mapper/RegistrulHcjcMapper.java
@@ -2,7 +2,6 @@ package com.lucian.dgaspc.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -15,24 +14,24 @@ import com.lucian.dgaspc.model.HotarareCJC;
 @Mapper
 public interface RegistrulHcjcMapper {
 	
-	@Select("select id, nrHotarare, DATE_FORMAT(dataHotarare, '%d.%m.%Y') as dataHotarare, emitent, DATE_FORMAT(dataComunicarii, '%d.%m.%Y') as dataComunicarii, idCompartiment, compartimentulAdresat, idPersoanaPrimire, persoanaPrimire, DATE_FORMAT(dataPrimirii, '%d.%m.%Y') as dataPrimirii, numeFisier, stadiu from registrul_hcjc where sters is null order by id desc")
+	@Select("select id, nrHotarare, DATE_FORMAT(dataHotarare, '%d.%m.%Y') as dataHotarare, emitent, DATE_FORMAT(dataComunicarii, '%d.%m.%Y') as dataComunicarii, numeFisier, stadiu from registrul_hcjc where sters is null order by id desc")
 	List<HotarareCJC> getHotarariCJC();
 	
-	@Select("select id, nrHotarare, DATE_FORMAT(dataHotarare, '%d.%m.%Y') as dataHotarare, emitent, DATE_FORMAT(dataComunicarii, '%d.%m.%Y') as dataComunicarii, idCompartiment, , idPersoanaPrimire, persoanaPrimire, DATE_FORMAT(dataPrimirii, '%d.%m.%Y') as dataPrimirii, numeFisier, stadiu from registrul_hcjc where id = #{id} and sters is null")
+	@Select("select id, nrHotarare, DATE_FORMAT(dataHotarare, '%d.%m.%Y') as dataHotarare, emitent, DATE_FORMAT(dataComunicarii, '%d.%m.%Y') as dataComunicarii, numeFisier, stadiu from registrul_hcjc where id = #{id} and sters is null")
 	HotarareCJC getHotarareCJCById(String id);
 	
-	@Insert("insert into registrul_hcjc(nrHotarare, dataHotarare, emitent, dataComunicarii, idCompartiment, compartimentulAdresat, idPersoanaPrimire, persoanaPrimire, dataPrimirii, fisier, numeFisier, stadiu)"
-			+ "values(#{nrHotarare}, STR_TO_DATE(#{dataHotarare}, '%d.%m.%Y'), #{emitent} , STR_TO_DATE(#{dataComunicarii}, '%d.%m.%Y'), #{idCompartiment}, #{compartimentulAdresat}, #{idPersoanaPrimire}, #{persoanaPrimire}, STR_TO_DATE(#{dataPrimirii}, '%d.%m.%Y'), null, null, #{stadiu})")
+	@Insert("insert into registrul_hcjc(nrHotarare, dataHotarare, emitent, dataComunicarii, fisier, numeFisier, stadiu)"
+			+ "values(#{nrHotarare}, STR_TO_DATE(#{dataHotarare}, '%d.%m.%Y'), #{emitent} , STR_TO_DATE(#{dataComunicarii}, '%d.%m.%Y'), null, null, #{stadiu})")
 	@Options(useGeneratedKeys=true, keyProperty="id")
 	Integer postHotarareCJC(HotarareCJC hotarareCJC);
 	
-	@Update("update registrul_hcjc set nrHotarare = #{nrHotarare}, dataHotarare = STR_TO_DATE(#{dataHotarare}, '%d.%m.%Y'), emitent = #{emitent}, dataComunicarii = STR_TO_DATE(#{dataComunicarii}, '%d.%m.%Y'), idCompartiment = #{idCompartiment}, compartimentulAdresat = #{compartimentulAdresat}, idPersoanaPrimire = #{idPersoanaPrimire}, persoanaPrimire = #{persoanaPrimire}, dataPrimirii = STR_TO_DATE(#{dataPrimirii}, '%d.%m.%Y'), stadiu = #{stadiu} where id = #{id}")
+	@Update("update registrul_hcjc set nrHotarare = #{nrHotarare}, dataHotarare = STR_TO_DATE(#{dataHotarare}, '%d.%m.%Y'), emitent = #{emitent}, dataComunicarii = STR_TO_DATE(#{dataComunicarii}, '%d.%m.%Y'), stadiu = #{stadiu} where id = #{id}")
 	void putHotarareCJC(HotarareCJC hotarareCJC);
 	
 	@Update("update registrul_hcjc set numeFisier = #{numeFisier}, fisier = #{continutFisier} where id = #{idHotarare}")
 	void incarcaFisier(String idHotarare, String numeFisier, byte[] continutFisier);
 	
-	@Select("select id, nrHotarare, DATE_FORMAT(dataHotarare, '%d.%m.%Y') as dataHotarare, emitent, DATE_FORMAT(dataComunicarii, '%d.%m.%Y') as dataComunicarii, idCompartiment, compartimentulAdresat, idPersoanaPrimire, persoanaPrimire, DATE_FORMAT(dataPrimirii, '%d.%m.%Y') as dataPrimirii, numeFisier, fisier, stadiu from registrul_hcjc where id = #{id} and sters is null ")
+	@Select("select id, nrHotarare, DATE_FORMAT(dataHotarare, '%d.%m.%Y') as dataHotarare, emitent, DATE_FORMAT(dataComunicarii, '%d.%m.%Y') as dataComunicarii, numeFisier, fisier, stadiu from registrul_hcjc where id = #{id} and sters is null ")
 	HotarareCJC getHotarareCuFisier(String id);
 	
 	/*
